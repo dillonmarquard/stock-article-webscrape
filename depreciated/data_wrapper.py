c7a6591069f6b53645abad473b23a1c1ad55ac5c
Dillon Marquard <dmarquard4@gmail.com>
2023-04-26 17:07:42 -0700

    re-do

--- depreciated/data_wrapper.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-
-class data_wrapper:
-    def __init__(self,tag,data,date,pChange=None):
-        self.tag = tag
-        self.date = date
-        self.pChange = pChange
-        self.data = data
--- pricedata/percent_gain.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-file = input() # Expecting Yahoo Finance Headers
-data = pd.read_csv(file)
-data['percent_change'] = (data['Close'] - data['Open']) / data['Open']
-data.to_csv(file,index=False)
