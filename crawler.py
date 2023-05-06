e1b8d074813b35c9937afd926e2381f2f35705ba
Dillon Marquard <dmarquard4@gmail.com>
2023-05-06 12:33:30 -0700

    updated to use requests-html and upon failure use selenium chrome driver; removed bug with title blacklist

--- crawler.py
+++ crawler.py
@@ -4,13 +4,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 
+from requests_html import HTMLSession # html-only sessions for faster scraping, but lose out on JS loaded content
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 import re
 import json
@@ -19,15 +19,28 @@ from tqdm import tqdm
 
 # Error Definitions
 class MaxRecursionError(Exception):
-    def __init__(self):
-        pass
+    def __str__(self):
+        return f'MaxRecursionError {self.url}'
+    def __init__(self,url:str):
+        self.url = url
 class ArticleNotFoundError(Exception):
+    def __str__(self):
+        return 'ArticleNotFoundError'
     def __init__(self):
         pass
 class AccessDeniedError(Exception):
+    def __str__(self):
+        return 'AccessDeniedError'
     def __init__(self):
         pass
 class NotAccessibleError(Exception):
+    def __str__(self):
+        return 'NotAccessibleError'
+    def __init__(self):
+        pass
+class LinkParseError(Exception):
+    def __str__(self):
+        return 'LinkParseError'
     def __init__(self):
         pass
 
@@ -37,6 +50,9 @@ class APIClient:
         return f'Chrome Webscraper {self.driver} ({self.chrome_driver_version})'
     
     def __init__(self):
+        # html-only session
+        self.session = HTMLSession()
+
         # webdriver setup
         chrome_options = Options()
         chrome_options.add_argument("start-maximized"); # https://stackoverflow.com/a/26283818/1689770
@@ -63,11 +79,11 @@ class APIClient:
         self.driver.set_page_load_timeout(5) # 8 for consistency, 4 for speed
 
         # recursion setup for auto-retry
-        self.MAX_DEPTH = 2
+        self.MAX_DEPTH = 1
         self.API_WAIT_SECONDS = 5
 
         # blacklisted link patterns ie: yahoo finance
-        self.link_blacklist = ['finance.yahoo','sec.gov','money.cnn','markets.businessinsider.com','google.com','marketwatch.com','github.com','youtube.com','fintel.io']
+        self.link_blacklist = ['finance.yahoo','sec.gov','money.cnn','markets.businessinsider.com','google.com','marketwatch.com','github.com','youtube.com','fintel.io','detroitnews.com','xml-sitemap']
 
         # configured lists
         self.not_found_titles = ['not*.found','404','can*.t*.find'] # all lower-case; any acceptable re pattern
@@ -75,35 +91,38 @@ class APIClient:
     ##################################
     
     # API
-    def get_stock_data(self,tag:str,date:str,num_links:int=25):
+    def get_stock_data(self,tag:str,date:str,num_links:int=25,progress=False):
         tmpdf = pd.DataFrame(columns=['date','tag','link','title','raw_html'])
         tmplinks = self.get_google_links(tag,date,num_links)
         
-        for i in range(len(tmplinks)):
+        if progress: # to update progress bar for testing
+            rng = tqdm(range(len(tmplinks)))
+        else:
+            rng = range(len(tmplinks))
+        for i in rng:
             try:
                 tmpdata = self.get_article_data(tmplinks[i])
                 if tmpdata != None:
                     tmpdata['date'] = date
                     tmpdata['tag'] = tag
                     tmpdf = pd.concat((tmpdf,pd.DataFrame([tmpdata],columns=['date','tag','link','title','raw_html'])),axis=0)
-                time.sleep(3) # to avoid api throttle, or spam detection
-            except:
-                pass # parse error, ignore fault
+            except Exception as e:
+                print('Error:',e) # parse error, ignore fault
         return tmpdf
-
+    
     def get_google_links(self,tag:str,date:str,num_links:int=25,recursion_depth=1):
         # Extract links from google search
         # Input: tag, date, num_links
         # Output: list of URLs
         url = "https://www.google.com/search?q={}+news+on%3A{}&num={}".format(tag,date,num_links)
-        res = self.get_html_from_url(url)
         try:
-            res = self.get_links(res)
+            tmp = self.session.get(url).html.absolute_links
+            res = self.filter_links(tmp)
         except:
             if recursion_depth <= self.MAX_DEPTH: # depth check for successive retry
                 res = self.get_google_links(tag,date,recursion_depth=recursion_depth+1)
             else:
-                raise MaxRecursionError
+                raise MaxRecursionError(url)
         return res
     
     def get_article_data(self,url:str,recursion_depth=1):
@@ -111,9 +130,12 @@ class APIClient:
         # Input: URL
         # Output: article data json
         try:
-            html = self.get_html_from_url(url)
+            if recursion_depth == 1:
+                html = self.get_html_from_url(url)
+            else:
+                html = self.get_js_html_from_url(url)
             data = {}
-            data['link'] = url # self identifier
+            data['link'] = url
             data['title'] = self.get_title(html)
             
             if max(map(lambda pattern : len(re.findall(pattern, data['title'].lower())), self.not_found_titles)):
@@ -126,21 +148,29 @@ class APIClient:
         except:
             if recursion_depth <= self.MAX_DEPTH: # depth check for successive retry
                 time.sleep(self.API_WAIT_SECONDS) # prevent api throttling on failure
-                data = self.get_article_data(url,recursion_depth=recursion_depth+1)
+                data = self.get_article_data(url,recursion_depth=recursion_depth+1) # try js on html-only failure
             else:
-                raise MaxRecursionError
-        
+                raise MaxRecursionError(url)
+    
     ##################################
     ##################################
 
     # Webdriver
-    def get_html_from_url(self,url:str):
+    def get_js_html_from_url(self,url:str):
         try:
             self.driver.get(url)
             return self.driver.page_source
         except:
             raise NotAccessibleError
-        
+
+    def get_html_from_url(self,url:str):
+        # https://requests.readthedocs.io/projects/requests-html/en/latest/
+        # requests-html is now trying to support js, but we will explore the capabilities later
+        try:
+            res = self.session.get(url)
+            return res.text
+        except:
+            raise NotAccessibleError    
 
     # HTML Processing Helper functions
     def get_text_v1_0(self,html:str):
@@ -157,16 +187,28 @@ class APIClient:
         res = re.sub('\s+',' ',soup.find_all('title')[0].text.replace('\n',' ').replace('\xa0',' ').replace('\'','’').replace('   ',' ').strip())
         return res
 
-    def get_links(self,html:str):
-        soup = BeautifulSoup(html, 'html.parser')
+    # only load JS when absolutely necessary to be more efficient and avoid throttling
+    # def get_links(self,html:str): 
+    #     soup = BeautifulSoup(html, 'html.parser')
+    #     links = []
+    #     for x in soup.find_all('a'):
+    #         try:
+    #             tmp = x['href'].replace('/url?esrc=s&q=&rct=j&sa=U&url=','')
+    #             if 'https://' == tmp[0:8] and min([re.findall(pattern,tmp) == [] for pattern in self.link_blacklist]): # check for blacklisted pattern
+    #                 links += [tmp]
+    #         except:
+    #             pass # parse error, ignore fault; bad data
+    #     return links
+
+    def filter_links(self,unfiltered:list):
         links = []
-        for x in soup.find_all('a'):
+        for x in unfiltered:
             try:
-                tmp = x['href'].replace('/url?esrc=s&q=&rct=j&sa=U&url=','')
+                tmp = x.replace('/url?esrc=s&q=&rct=j&sa=U&url=','')
                 if 'https://' == tmp[0:8] and min([re.findall(pattern,tmp) == [] for pattern in self.link_blacklist]): # check for blacklisted pattern
-                    links += [tmp]
+                    links += [tmp] # only add non-blacklisted urls
             except:
-                pass # parse error, ignore fault; bad data
+                raise LinkParseError # parse error, ignore fault; bad data
         return links
 
     def __del__(self):
